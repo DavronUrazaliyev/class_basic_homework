c734d3813917f3ef254a3028e6cb7f8373ad3a58
Davron17 <dorazaliyev75@gmail.com>
2023-11-15 14:51:19 +0500

    objects to list

--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -1,7 +1,7 @@
 from create_one_attribute import Person
-
-#Create an object named "p1" whose name is "Anvar"
-#Create an object named "p2" whose name is "Shavkat"
-#Create an object named "p3" whose name is "Jasur"
-
-#Add these objects to the "persons" named list
+p1 = Person(name="Anvar")
+p2 = Person(name="Shavkat")
+p3 = Person(name="Jasur")
+Persons=[p1,p2,p3]
+for person in Persons:
+    print(person.name)
